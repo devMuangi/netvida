8543d794926d8b8d3522c59b79141f066628c47e
devMuangi <devmuangi@gmail.com>
2021-06-20 18:34:39 +0300

    fetch all movies

---
+++ src/Row.js
@@ -0,0 +1,32 @@
+import React,{ useEffect, useState} from 'react';
+import axios from './axios';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl}) {
+    const [movies, setmovies] = useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setmovies(request.data.results);
+            return request;
+        }
+        fetchData();
+
+    }, [fetchUrl]);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie=>(
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+            </div>
+            
+        </div>
+    )
+}
+
+export default Row;
